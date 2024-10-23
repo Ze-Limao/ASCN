63453bb23de945c02afbc61a4ccd286d89ad9c5a
Ze-Limao <jeswitch.24@gmail.com>
2024-10-24 00:13:36 +0100

    Refactor Dockerfile and script.sh for better organization and efficiency

--- docker/script.sh
+++ docker/script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo ">>> Running migration"
-python manage.py migrate
+venv/bin/python manage.py migrate
 
 if [ "$SEED" == "true" ]; then
   echo ">>> Running seed"
-  python seed.py
+  venv/bin/python seed.py
 fi
 
 echo ">>> Starting app..."
-python manage.py runserver 0.0.0.0:8000
+venv/bin/python manage.py runserver 0.0.0.0:8000
